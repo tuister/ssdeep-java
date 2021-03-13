895d04ceb798ca6c50304c0e226603b1972268bf
tuister <tuister@163.com>
2021-03-13 11:46:29 +0800

    Add a command line tool

---
+++ src/main/java/tech/tuister/ssdeep4j/Fuzzy.java
@@ -0,0 +1,32 @@
+package tech.tuister.ssdeep4j;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * @author tuister 2021/3/13 11:35
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class Fuzzy {
+
+    public static void main(String[] args) {
+
+        if (args == null || args.length == 0) {
+            System.out.println("Usage: java -jar ssdeep.jar path");
+            System.exit(-1);
+        }
+
+        for (String arg : args) {
+            Path path = Paths.get(arg);
+            if (Files.isDirectory(path)) {
+                System.out.println(arg + ": Is a directory");
+            } else {
+                String s = FuzzyHashing.fuzzyHashFilename(arg);
+                System.out.println(s + ",\"" + arg + "\"");
+            }
+        }
+
+    }
+}
